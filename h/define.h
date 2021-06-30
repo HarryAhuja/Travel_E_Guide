0b18fac667f67d7cc180d105ff97814d6502a330
Harry Ahuja <harryahuja2@gmail.com>
2021-06-30 23:04:30 +0530

    Header file for Constants and Defines

---
+++ h/define.h
@@ -0,0 +1,12 @@
+#ifndef __DEFINE_H__
+#define __DEFINE_H__
+
+#define MAX_N           1001
+#define MAX_M           11
+#define MAX_FRIENDS     21
+#define MAX_PACKAGES    65536
+#define bool            int
+#define true            1
+#define false           0
+
+#endif
